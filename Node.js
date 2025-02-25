fe25687089df9ba50fd916d1c04de8b83e119fca
ubv-d <jcfnulhu@gmail.com>
2025-02-25 15:37:53 +0800

    更新 Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,7 @@ const app = express();
 
 // 模拟生成新的二维码图片URL
 app.get('/generate-qrcode', (req, res) => {
-    const newQrcodeUrl = 'https://example.com/new_qrcode.png'; // 新的二维码图片URL
+    const newQrcodeUrl = 'm.0089525.com_qrcodenotfound.13498449.jpg'; // 新的二维码图片URL
     res.json({ qrcodeUrl: newQrcodeUrl });
 });
 
